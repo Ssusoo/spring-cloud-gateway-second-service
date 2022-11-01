c2517763e779ac2339caa8709f5a87d6dbbeab48
김수환 <zzanggoon8@hiconsy.com>
2022-11-01 19:10:58 +0900

    filter Config Add

---
+++ src/main/java/me/ssu/springcloudgatewaysecondservice/controller/config/FilterConfig.java
@@ -0,0 +1,21 @@
+package me.ssu.springcloudgatewaysecondservice.controller.config;
+
+import org.apache.http.client.methods.RequestBuilder;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/second-service/**")
+						.filters(f -> f.addRequestHeader("second-request", "second-request-header")
+								.addResponseHeader("second-response", "second-response-header"))
+						.uri("/http://localhost:8082/"))
+				.build();
+	}
+}
