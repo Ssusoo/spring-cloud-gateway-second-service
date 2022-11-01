8a532cfd478f98bc1d3989cf3aa8e6958d37d4d8
김수환 <zzanggoon8@hiconsy.com>
2022-11-01 18:33:39 +0900

    first commit

---
+++ src/test/java/me/ssu/springcloudgatewaysecondservice/SpringCloudGatewaySecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ssu.springcloudgatewaysecondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewaySecondServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
