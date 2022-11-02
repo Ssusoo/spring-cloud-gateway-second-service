cb6066d50ff46b430c512361cd26287a51531f03
김수환 <zzanggoon8@hiconsy.com>
2022-11-02 11:59:45 +0900

    lombok gradle dependency Add

--- src/main/java/me/ssu/springcloudgatewaysecondservice/controller/SecondServiceController.java
+++ src/main/java/me/ssu/springcloudgatewaysecondservice/controller/SecondServiceController.java
@@ -1,5 +1,6 @@
 package me.ssu.springcloudgatewaysecondservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,16 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 // http://localhost:8082/second-service/welcome
 @RestController
 @RequestMapping("/second-service")
+@Slf4j
 public class SecondServiceController {
 
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome to the Second Service";
-	}
-
-	@GetMapping("/message")
-	public String message(@RequestHeader("second-request") String header) {
-		System.out.println(header);
-		return "Hello World in First Service";
 	}
 }
